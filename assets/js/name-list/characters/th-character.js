911a2f0e022fa3bf6583afb9a7f4767cd0eea426
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-02-27 19:42:07 -0500

    feat: add the missing characters
    Also revise the frame count.

--- assets/js/name-list/characters/th-character.js
+++ assets/js/name-list/characters/th-character.js
@@ -4,7 +4,7 @@ import {
   styleMap,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
 
-import thMapping from "./th-mapping.js";
+import { thMapping, maiEk, maiTho, maiTri } from "./th-mapping.js";
 
 export class ThaiCharacter extends LitElement {
   static properties = {
@@ -39,6 +39,33 @@ export class ThaiCharacter extends LitElement {
     return renderFunc(styles);
   }
 
+  getFrameCount(character) {
+    if (character === "า") {
+      return 80;
+    } else if (character === "เ") {
+      return 80;
+    } else if (character === "ิ") {
+      return 80;
+    } else if (character === "ี") {
+      return 80;
+    } else if (character === maiEk) {
+      return 80;
+    } else if (character === maiTho) {
+      return 80;
+    } else if (character === maiTri) {
+      return 80;
+    } else if (character === "ุ") {
+      return 80;
+    } else if (character === "ู") {
+      return 80;
+    } else if (character === "ั") {
+      return 80;
+    } else {
+      // default number of frames.
+      return 200;
+    }
+  }
+
   updated() {
     const svgEle = this.renderRoot.querySelector("#th-char");
 
@@ -55,7 +82,7 @@ export class ThaiCharacter extends LitElement {
 
     this.animator = new Vivus(
       svgEle,
-      { duration: this.duration, type: "oneByOne" },
+      { duration: this.getFrameCount(this.name), type: "oneByOne" },
       (vivusObj) => {
         if (this.loop && vivusObj.getStatus() === "end") {
           this.loopingTimer = setTimeout(() => {
